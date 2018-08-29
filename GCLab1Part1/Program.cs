f33d3afa0ee409d59a2e688a5380c283d9f32802
Jessica Billingslea <jessicrr@gmail.com>
2018-08-28 21:43:34 -0400

    Reworded output for splitting the digits

--- GCLab1Part1/Program.cs
+++ GCLab1Part1/Program.cs
@@ -6,72 +6,80 @@ namespace GCLab1Part1
     {
         public static void Main(string[] args)
         {
-            //Assigning variables 
+            // Assigning variables 
             int input1, input2, firstnum1, firstnum2, secondnum1, secondnum2, thirdnum1, thirdnum2, total1, total2, total3;
 
-            //User input for number1
+            // User input for number1
             Console.WriteLine("Please enter a 3 digit number:");
             input1 = Convert.ToInt32(Console.ReadLine());
 
-                //Validation of user's input for number1
+                // Validation of user's input for number1
             while (input1 < 100 || input1 >999)
             {
                 Console.WriteLine("The number you entered is invalid, please re-enter a three digit number.");
                 input1 = Convert.ToInt32(Console.ReadLine());
             }
 
-            //User input for number2 
+            // User input for number2 
             Console.WriteLine("Please enter another 3 digit number:");
             input2 = Convert.ToInt32(Console.ReadLine());
            
-                //Validation for number2
+                // Validation of user's input for number2
             while (input2 < 100 || input2 > 999)
             {
-                Console.WriteLine("The number you entered is invalid, please reenter a three digit number.");
+                Console.WriteLine("The number you entered is invalid, please re-enter a three digit number.");
                 input2 = Convert.ToInt32(Console.ReadLine());
             }
 
-            //splitting the digits
+            // Splitting the digits
             firstnum1 = (input1 / 100) % 10;
             firstnum2 = (input2 / 100) % 10;
-            Console.WriteLine("The numbers in the hundreds place for the first and second 3 digit numbers are {0} and {1}, respectively", firstnum1, firstnum2);
+            Console.WriteLine();
+            Console.WriteLine("The numbers in the hundreds place for each 3 digit number are {0} and {1}, respectively", firstnum1, firstnum2);
             secondnum1 = (input1 / 10) % 10;
             secondnum2 = (input2 / 10) % 10;
-            Console.WriteLine("The numbers in the tens place for the first and second 3 digit numbers arew {0} and {1}, respectively", secondnum1, secondnum2);
+            Console.WriteLine("The numbers in the tens place for each 3 digit number are {0} and {1}, respectively", secondnum1, secondnum2);
             thirdnum1 = input1 % 10;
             thirdnum2 = input2 % 10;
-            Console.WriteLine("The numbers in the ones place for the first and second 3 digit numbers are {0} and {1}, respectively", thirdnum1, thirdnum2);
+            Console.WriteLine("The numbers in the ones place for each 3 digit number are {0} and {1}, respectively", thirdnum1, thirdnum2);
+            Console.WriteLine();
 
-            //adding the ones, tens and hundreds
+            // Adding the ones, tens and hundreds places together
             total1 = firstnum1 + firstnum2;
             Console.WriteLine("The sum of the numbers in the hundreds place is {0}", total1);
             total2 = secondnum1 + secondnum2;
             Console.WriteLine("The sum of the numbers in the tens place is {0}", total2);
             total3 = thirdnum1 + thirdnum2;
             Console.WriteLine("The sum of the numbers in the ones place is {0}", total3);
+            Console.WriteLine();
 
-            //Calling the method and printing the result
-            if (Result(total1, total2, total3) == true){
-                Console.WriteLine("True, the sum of the integers in the ones, tens and hundreds place are all equal.");
+            // Calling the Result method and printing the result
+            if (Result(total1, total2, total3) == true)
+            {
+                Console.WriteLine("True, the sum of the numbers in the ones, tens and hundreds place are all equal.");
             }
-            else{
-                Console.WriteLine("False, the sum of the integers in the ones, tens and hundreds place are not equal.");
+            else
+            {
+                Console.WriteLine("False, the sum of the numbers in the ones, tens and hundreds place are not equal.");
             }
-
         }
-        //Declaring a method
-        static bool Result(int a, int b, int c){
-            if ( a == b){
+        // Declaring a method (Result) to determine if all three totals are the same
+        static bool Result(int a, int b, int c)
+        {
+            if ( a == b)
+            {
                 if (b == c)
                 {
                     return true;
                 }
-                    else{
-                        return false;
-                    }
+                else
+                {
+                    return false;
                 }
+            }
 
-            else {
+            else 
+            {
                 return false;
             }
         }
